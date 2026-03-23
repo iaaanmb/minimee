12964434518b09361d53d978c92386a5a3e4f91e
iaaanmb <iansantiagomarbusmar@gmail.com>
2026-03-22 22:13:10 -0500

    Add files via upload

---
+++ netlify/functions/log-pedido.js
@@ -0,0 +1,43 @@
+exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const SHEETS_URL = process.env.SHEETS_URL;
+    if (!SHEETS_URL) throw new Error('SHEETS_URL not configured');
+
+    const data = JSON.parse(event.body);
+
+    const response = await fetch(SHEETS_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ ok: true })
+    };
+  } catch (err) {
+    console.error('log-pedido error:', err);
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
